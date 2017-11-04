f678c15f818a91606b5d1f095d90064595dcd56c
502581120 <furog.jerome@ge.com>
2017-11-05 01:22:38 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// set up ========================
+var express  = require('express');
+var app      = express();                   // create our app w/ express
+
+// configuration =================
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+
+// listen (start app with node server.js) ======================================
+app.listen(8080);
+console.log("App listening on port 8080");
